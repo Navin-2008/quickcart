8bc090b8e9ec0888bcb4512a2385d0a28d1f43a8
Navin-2008 <navin.lv.s.141@kalvium.community>
2026-05-05 11:24:43 +0530

    Revert "part-1"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import Header from './components/Header';
-import ProductList from './components/ProductList';
-import { products } from './data/products';
-import './styles/App.css';
-
 function App() {
   return (
     <div className="app">
-      <Header />
-      <main className="main-content">
-        <ProductList products={products} />
-      </main>
+      <h1>QuickCart</h1>
     </div>
   );
 }
